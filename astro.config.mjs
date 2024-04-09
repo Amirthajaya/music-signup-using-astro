adc2c9731a9836974ad69fc072d01659aba865a7
unknown <amirthanila30@gmail.com>
2024-04-09 10:34:16 +0530

    first-commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({routes: [
+    { path: '/', component: '../' }, // Your main page
+    { path: '/Sign Up', component: 'SignUpForm.astro' }, // Your sign-up page
+  ],});
+
